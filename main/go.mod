8ef1be1df4766d62e62ff7e1ceba309c383d0a36
Morgan Slater <45826559+slatermorgan@users.noreply.github.com>
2021-05-22 08:25:09 +0100

    Implement CSV traversal

---
+++ main/go.mod
@@ -0,0 +1,3 @@
+module github.com/slatermorgan/csv-conv
+
+go 1.16
---
+++ main/main.go
@@ -0,0 +1,46 @@
+package main
+import (
+    "fmt"
+	"encoding/csv"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("products.csv")
+	reader := csv.NewReader(file)
+	reader.LazyQuotes = true
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Associative array [ PLU => ItemCategory ]
+	productMap := make(csvmap.CsvMap)
+
+	for {
+		line, err := reader.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		product := csvmap.Product{
+			SKU:      line[0],
+			PLU:      line[1],
+			Name:     line[2],
+			Size:     line[3],
+			SizeSort: line[4],
+		}
+
+		item := csvmap.Item{
+			SKU:  product.SKU,
+			Size: product.Size,
+		}
+
+}
